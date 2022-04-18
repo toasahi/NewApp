84c493ec1ed1d8a0f2f301fcd580fafc1795aa41
toasahi <asa1313to@gmail.com>
2022-04-18 16:20:26 +0900

    Add dummy data

---
+++ src/dummy/data.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    id: '1',
+    title: '星野源',
+    desc: 'ジャンプコミックスで大人気の漫画がついにアニメ化。星野源が新曲を書き下ろしました。',
+    thumbnail: 'SPY Familyに楽曲提供',
+    category: 'anime',
+  },
+  {
+    id: '2',
+    title: '星野源',
+    desc: 'ジャンプコミックスで大人気の漫画がついにアニメ化。星野源が新曲を書き下ろしました。',
+    thumbnail: 'SPY Familyに楽曲提供',
+    category: 'anime',
+  },
+  {
+    id: '3',
+    title: '星野源',
+    desc: 'ジャンプコミックスで大人気の漫画がついにアニメ化。星野源が新曲を書き下ろしました。',
+    thumbnail: 'SPY Familyに楽曲提供',
+    category: 'anime',
+  },
+  {
+    id: '4',
+    title: '星野源',
+    desc: 'ジャンプコミックスで大人気の漫画がついにアニメ化。星野源が新曲を書き下ろしました。',
+    thumbnail: 'SPY Familyに楽曲提供',
+    category: 'anime',
+  },
+];
