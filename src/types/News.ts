9ebd9936543175572c0ab97de40f2e0c8a19b83d
toasahi <asa1313to@gmail.com>
2022-04-18 15:06:18 +0900

    Add axios & API type

---
+++ src/types/News.ts
@@ -0,0 +1,17 @@
+export type News = {
+  articles: [
+    {
+      source: {
+        id: null;
+        name: string;
+      };
+      author: null;
+      title: string;
+      description: string;
+      url: string;
+      urlToImage: string;
+      publishedAt: string;
+      content: string;
+    },
+  ];
+};
