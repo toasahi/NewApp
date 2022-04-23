e23344c24ffb04f4d2f00d3b92e6a254b087e369
toasahi <asa1313to@gmail.com>
2022-04-23 22:55:09 +0900

    react navigation 追加

--- App.tsx
+++ App.tsx
@@ -3,14 +3,23 @@ import { SearchBar } from './src/components/SearchBar';
 import { Screen } from './src/components/Screen';
 import { BreakingNews } from './src/components/BreakingNews';
 import { FeaturedNews } from './src/components/FeaturedNews';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+// import { Home } from './src/components/Home';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <Screen>
-      <SearchBar />
-      <FeaturedNews />
-      <BreakingNews />
-    </Screen>
+    <NavigationContainer>
+      <Screen>
+        <SearchBar />
+        <FeaturedNews />
+        <BreakingNews />
+        <Stack.Navigator>
+          <Stack.Screen name="Home" getComponent={() => require('./src/components/Home').Home} />
+        </Stack.Navigator>
+      </Screen>
+    </NavigationContainer>
   );
 };
 
