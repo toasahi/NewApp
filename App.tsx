1df2b2883b830872f07786d2c2e649160671a6e5
toasahi <asa1313to@gmail.com>
2022-04-23 22:25:30 +0900

    comment 削除

--- App.tsx
+++ App.tsx
@@ -3,7 +3,6 @@ import { SearchBar } from './src/components/SearchBar';
 import { Screen } from './src/components/Screen';
 import { BreakingNews } from './src/components/BreakingNews';
 import { FeaturedNews } from './src/components/FeaturedNews';
-// import { SmallCard } from './src/components/SmallCard';
 
 const App = () => {
   return (
