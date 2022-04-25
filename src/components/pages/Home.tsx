b303ddd3339a8eb7c9ac1c46e2be46b843246294
toasahi <asa1313to@gmail.com>
2022-04-25 18:51:20 +0900

    folder 分割

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from 'react';
-import { SearchBar } from './SearchBar';
-import { FeaturedNews } from './FeaturedNews';
-import { BreakingNews } from './BreakingNews';
-import { Screen } from './Screen';
+import { SearchBar } from '../SearchBar';
+import { FeaturedNews } from '../FeaturedNews';
+import { BreakingNews } from '../BreakingNews';
+import { Screen } from '../Screen';
 
 export const Home = memo(() => {
   return (
